bb0a203b795fd3e83efc05c614d50395ae563189
Emilia Muresan <emilia.muresan@moduscreate.com>
2020-08-10 12:41:33 +0300

    chore: good commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import RouteHandler from "./Router.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import LandingPage from "./Components/LandingPage";
 import UserContextProvider from "./context/UserContext";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4001/gql",
-	cache: new InMemoryCache(),
-	onError: ({ networkError, graphQLErrors }) => {
-		console.log("graphQLErrors", graphQLErrors);
-		console.log("networkError", networkError);
-	},
+  uri: "http://localhost:4001/gql",
+  cache: new InMemoryCache(),
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  },
 });
 
 export default function App() {
-	return (
-		<ApolloProvider client={client}>
-			<UserContextProvider>
-				<RouteHandler />
-			</UserContextProvider>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <UserContextProvider>
+        <RouteHandler />
+      </UserContextProvider>
+    </ApolloProvider>
+  );
 }
